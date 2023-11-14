d28e7b6012e7579b8279d0597c278217681055f6
Aashish Kushwah <119287051+Aashu623@users.noreply.github.com>
2023-11-15 00:07:54 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="copyright">
+          &copy; 2023 All Rights Reserved by
+          <a
+            href="/"
+            className="copyright-link"
+          >
+            disenthrall
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
